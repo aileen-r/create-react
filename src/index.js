1595942d91997d35aea8b7013b6a3d6b5373f338
Aileen Rae <aileenlrae@gmail.com>
2019-11-08 18:40:07 +0000

    Append FCC script when ?fcc query is present

--- src/index.js
+++ src/index.js
@@ -7,6 +7,14 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
+if (window.location.search === '?fcc') {
+  const scriptEl = document.createElement('script');
+  scriptEl.async = true;
+  scriptEl.src =
+    'https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js';
+  document.getElementsByTagName('body')[0].appendChild(scriptEl);
+}
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
